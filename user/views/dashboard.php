cc541e7719d47e0b511ce6fcde4229684e45fbf1
colelezzz <islesleen@gmail.com>
2025-06-01 23:26:10 +0800

    FE | Added Navbar for User and Content for Userdashboard

---
+++ user/views/dashboard.php
@@ -0,0 +1,66 @@
+<div id="dashboard" class="container">
+  <div id="announcement" class="container py-4 mb-3 mt-2">
+    <div class="row">
+      <div class="col-12">
+        <div class="heading mb-3" style="font-size: clamp(2.2rem, 5vw, 3rem);">ANNOUNCEMENT</div>
+        <hr style="border-top: 3px solid #000; opacity: 1;">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-9 mx-4 my-2">
+              <div class="rounded-box">
+                “Gym closed on April 20 for maintenance.”
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-9 mx-4 my-2">
+              <div class="rounded-box">
+                “New Yoga Class starts April 15.”
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-9 mx-4 my-2">
+              <div class="rounded-box">
+                “Gym closed on April 24 for maintenance.”
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="engagement" class="container py-4 mb-3 mt-3">
+    <div class="row">
+      <div class="col-12">
+        <div class="heading mb-3" style="font-size: clamp(2.2rem, 5vw, 3rem);">YOUR ENGAGEMENT</div>
+        <hr style="border-top: 3px solid #000; opacity: 1;">
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <div class="heading mb-3 mt-2 mx-3" style="font-size: clamp(1.3rem, 4vw, 1.8rem);">MEMBERSHIP STATUS</div>
+          <div class="mt-1 mx-4" style="font-family:var(--primaryFont); font-size: 1.3rem; font-weight: bold;">
+            Membership Plan</div>
+          <div class="mx-4 mb-2" style="font-family: var(--primaryFont); font-size: clamp(1rem, 3vw, 1.3rem);">Gold
+          </div>
+          <div class="mt-1 mx-4" style="font-family:var(--primaryFont); font-size: 1.3rem; font-weight: bold;">
+            Expiry Date</div>
+          <div class="mx-4 mb-2" style="font-family: var(--primaryFont); font-size: clamp(1rem, 3vw, 1.3rem);">
+            2025-04-30</div>
+        </div>
+        <div class="col-12 col-md-6">
+          <div class="heading mb-3 mt-2 mx-3" style="font-size: clamp(1.3rem, 4vw, 1.8rem);">ATTENDANCE HISTORY</div>
+          <div class="mt-1 mx-4" style="font-family:var(--primaryFont); font-size: 1.3rem; font-weight: bold;">
+            Check-ins</div>
+          <div class="mx-4 mb-2" style="font-family: var(--primaryFont); font-size: clamp(1rem, 3vw, 1.3rem);">
+            23</div>
+          <div class="mt-1 mx-4" style="font-family:var(--primaryFont); font-size: 1.3rem; font-weight: bold;">
+            Streak</div>
+          <div class="mx-4 mb-2" style="font-family: var(--primaryFont); font-size: clamp(1rem, 3vw, 1.3rem);">
+            5</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
