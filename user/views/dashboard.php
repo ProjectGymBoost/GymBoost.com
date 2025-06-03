d9de1bfa347c002ad23ca41d26bf6d7fd0dacc16
colelezzz <islesleen@gmail.com>
2025-06-03 15:12:34 +0800

    FE | Fixed spacing and Removed AI Chatbot Tab

--- user/views/dashboard.php
+++ user/views/dashboard.php
@@ -2,10 +2,10 @@
   <div id="announcement" class="container px-0 py-4 mb-3 mt-2">
     <div class="row">
       <div class="col-12">
-        <div class="heading mb-3">ANNOUNCEMENT</div>
-        <hr style="border-top: 3px solid #000; opacity: 1;">
+        <div class="heading">ANNOUNCEMENT</div>
+        <hr style="border-top: 3px solid #000; opacity: 1; margin:0;">
         <div class="container">
-          <div class="row justify-content-center">
+          <div class="row justify-content-center mt-2">
             <div class="col-12 col-md-8 mx-4 my-2">
               <div class="rounded-box">
                 “Gym closed on April 20 for maintenance.”
@@ -34,10 +34,10 @@
   <div id="engagement" class="container px-0 py-4 mb-3 mt-3">
     <div class="row">
       <div class="col-12">
-        <div class="heading mb-3">YOUR ENGAGEMENT</div>
-        <hr style="border-top: 3px solid #000; opacity: 1;">
+        <div class="heading">YOUR ENGAGEMENT</div>
+        <hr style="border-top: 3px solid #000; opacity: 1; margin:0;">
       </div>
-      <div class="row">
+      <div class="row mt-2">
         <div class="col-12 col-md-6">
           <div class="subheading mb-3 mt-2 mx-3">MEMBERSHIP STATUS</div>
           <div class="mt-1 mx-4" style="font-weight:bold">
@@ -67,25 +67,4 @@
       </div>
     </div>
   </div>
-
-  <div id="aichat-container">
-    <div id="aichat-button" onclick="toggleChat()">
-      <img src=".././assets/img/logo/officialLogo.png" alt="Chat Icon">
-    </div>
-
-    <!-- tab -->
-    <div id="chat-tab">
-      <div class="chat-body">
-      </div>
-    </div>
-  </div>
-
 </div>
-
-<script>
-  function toggleChat() {
-  const chatTab = document.getElementById("chat-tab");
-  chatTab.classList.toggle("open"); 
-}
-
-</script>
