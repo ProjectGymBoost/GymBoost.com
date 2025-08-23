8e7ce741428d5ff7f92f5740b563d1ecb21153d6
Lira Angela Torralba <liratorralbasta@gmail.com>
2025-08-23 14:57:26 +0800

    Admin BADGE -> BADGE NAME

--- admin/badges.php
+++ admin/badges.php
@@ -48,7 +48,7 @@ include("../assets/php/processes/admin/badges.php");
                         <thead class="align-middle">
                             <tr>
                                 <th scope="col">ID</th>
-                                <th scope="col">BADGE</th>
+                                <th scope="col">BADGE NAME</th>
                                 <th scope="col">DESCRIPTION</th>
                                 <th scope="col">REQUIREMENT VALUE</th>
                                 <th scope="col">ICON</th>
