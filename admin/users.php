08b224afe7801259027d967e243fcd7ed04ba002
Kaye Gamana <kayecgamana@gmail.com>
2025-09-06 20:39:35 +0800

    BE | Refinements

--- admin/users.php
+++ admin/users.php
@@ -264,7 +264,7 @@ include("../assets/php/processes/admin/users.php");
                 clearTimeout(debounceTimer);
                 debounceTimer = setTimeout(() => {
                     form.submit();
-                }, 500);
+                }, 1000);
             });
 
             // FOCUS BACK TO SEARCH INPUT AFTER RELOAD
