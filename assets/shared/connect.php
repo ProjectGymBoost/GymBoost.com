f01a9e4cc9dcaaf9258f1392aa14acb5e690dab5
Lira Angela Torralba <liratorralbasta@gmail.com>
2025-10-10 14:52:38 +0800

    BE | Badge refinements

--- assets/shared/connect.php
+++ assets/shared/connect.php
@@ -2,7 +2,7 @@
   $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "newgymboost";
+	$db = "gymboost";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
